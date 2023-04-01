a53f8a4853ac649c07565eae3ef9fd47e2eea8ac
Biramax <biramax@biramax>
2023-04-02 00:44:06 +0300

    Добавил логирование к калькулятору из предыдущего задания

--- task1_3.java
+++ task1_3.java
@@ -3,9 +3,24 @@
 */
 
 import java.util.Scanner;
+import java.util.logging.*;
 
 public class task1_3 {
+
+    private static Logger logger = Logger.getLogger(task2_5.class.getName());
+    private static String logFileName = "task1_3__log.txt";
+
     public static void main(String[] args) {
+
+        // Настройка обработчика файлового логгера
+        try {
+            FileHandler fileHandler = new FileHandler(logFileName);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            logger.addHandler(fileHandler);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Не удалось настроить логгер:\n", e+"\n");
+        }
         
         Scanner in = new Scanner(System.in);
 
@@ -13,12 +28,18 @@ public class task1_3 {
         int a = in.nextInt();
         in.nextLine();
 
+        logger.info("Ввели первое число: "+a+"\n");
+
         System.out.printf("Введите второе число: ");
         int b = in.nextInt();
         in.nextLine();
 
+        logger.info("Ввели второе число: "+b+"\n");
+
         System.out.printf("Введите действие (+,-,*,/): ");
         String o = in.nextLine();
+
+        logger.info("Ввели требуемое действие: "+o+"\n");
 
         in.close();
 
@@ -32,5 +53,9 @@ public class task1_3 {
         }
     
         System.out.println("Результат: "+res);
+
+        logger.info("Получили результат: "+res+"\n");
     }
 }
+
+
